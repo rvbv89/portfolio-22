4d048b5ae8dd8e708b773aadd7aca8a085a8679a
rvbv89 <garzotto5389@gmail.com>
2022-09-09 14:40:23 -0500

    added animations to intro card

--- src/MainCard.jsx
+++ src/MainCard.jsx
@@ -21,7 +21,16 @@ export const MainCard = ({ projectRef }) => {
       ref={projectRef}
       className="box-border flex flex-col mx-auto max-w-screen-sm md:max-w-full lg:flex-row items-center min-h-screen text-slate-50"
     >
-      <div className="flex-auto basis-1/2 m-4 md:m-20 p-6 border border-solid border-fuchsia-600 border-4 rounded-md">
+      <motion.div
+        className="flex-auto basis-1/2 m-4 md:m-20 p-6 border border-solid border-fuchsia-600 border-4 rounded-md"
+        initial={{
+          x: -800,
+        }}
+        animate={{
+          x: 0,
+        }}
+        transition={{ duration: 2.5 }}
+      >
         <h1 className="text-8xl mx-auto md:text-8xl m-6 py-4 font-display">
           Hi, I'm <span>Ivan.</span>
         </h1>
@@ -29,13 +38,22 @@ export const MainCard = ({ projectRef }) => {
           I'm a frontend developer. I love HTML, CSS, JavaScript and React. I'm
           currently learning Node/Express as well.
         </p>
-      </div>
-      <motion.div className="flex flex-col basis-1/2 p-4 mx-auto">
+      </motion.div>
+      <motion.div
+        className="flex flex-col basis-1/2 p-4 mx-auto"
+        initial={{
+          x: 700,
+        }}
+        animate={{
+          x: 0,
+        }}
+        transition={{ duration: 2.5, delay: 0.75 }}
+      >
         <div className="mb-6">
           <img
             className="max-w-sm mx-auto"
             src={require("./img/ilg.png")}
-            alt="A photo of the page author"
+            alt="The page author"
           />
         </div>
 
